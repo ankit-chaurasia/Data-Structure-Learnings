493879ff8d0b09c108108a8293e3c725f876fc99
Chaurasia <Ankit.Chaurasia@honeywell.com>
2020-05-25 23:12:04 +0530

    linked list insert and delete

---
+++ linked-list/index.js
@@ -0,0 +1,105 @@
+const Node = require("./utils/node");
+
+class LinkedList {
+  head = null;
+  count = 0;
+  push = (element) => {
+    const node = Node(element);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.count++;
+    console.log("Push: ", element, " Size: ", this.size(), this.toString());
+  };
+
+  pop = () => {
+    if (this.size() === 1) {
+      this.head = null;
+    } else {
+      let previous = null;
+      let current = this.head;
+      while(current.next !== null) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = null;
+    }
+    this.count--;
+    console.log("Pop", "Size: ", this.size(), this.toString());
+  };
+
+  deleteAt = (index) => {
+    if(index >=0 && index <= this.size()) {
+      if(index === 0) {
+        this.head = this.head.next;
+      } else {
+        let previous = null;
+        let current = this.head;
+        for(let i = 1; i <= index && current.next !== null; i++ ) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.count--;
+    } else {
+      console.log('Invalid index', index);
+    }
+    console.log("Deleted at:", index, " Size: ", this.size(), this.toString());
+  }
+
+  insertAt = (index, element) => {
+    if (index >= 0 && index <= this.size()) {
+      const node = Node(element);
+      if (index === 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        let current = this.head;
+        for (let i = 1; i < index && current.next !== null; i++) {
+          current = current.next;
+        }
+        node.next = current.next;
+        current.next = node;
+      }
+      this.count++;
+    } else {
+      console.log("Invalid index", index);
+    }
+    console.log("Insert at:", index, "element", element, " Size: ", this.size(), this.toString());
+  };
+
+  size = () => this.count;
+
+  toString = () => {
+    if (!this.head) {
+      return "";
+    }
+    let current = this.head;
+    let objString = `${current.element}`;
+    while (current.next !== null) {
+      current = current.next;
+      objString = `${objString}, ${current.element}`;
+    }
+    return `** ${objString} **`;
+  };
+}
+
+module.exports = LinkedList;
+
+const linkedList = new LinkedList();
+
+linkedList.insertAt(0, 5);
+linkedList.push(10);
+linkedList.insertAt(1, 15);
+linkedList.push(20);
+linkedList.insertAt(1, 25);
+linkedList.pop();
+linkedList.deleteAt(0);
+linkedList.deleteAt(3);
---
+++ linked-list/utils/node.js
@@ -0,0 +1,4 @@
+module.exports = (element) => ({
+  element,
+  next: null,
+});
