cf6752c57ced37ba49226f234797f3cc43967af9
Ankit Chaurasia <chaurasiaankit07@gmail.com>
2021-04-22 16:15:52 +0530

    added binary heaps and priority queue

--- binary-heaps/max-binary-heap.js
+++ binary-heaps/max-binary-heap.js
@@ -36,10 +36,52 @@ class MaxBinaryHeap {
     }
     bubbleUp(this.value.length - 1);
   }
+
+  heapify() {
+    let parentIndex = 0;
+    const length = this.value.length;
+    while(true){
+      const element = this.value[parentIndex];
+      let swapIndex = null;
+      let leftElement = null;
+      let rightElement = null;
+      const leftIndex = 2*parentIndex + 1;
+      const rightIndex = 2*parentIndex + 2;
+      if(leftIndex < length) {
+        leftElement = this.value[leftIndex];
+        if(leftElement > element) {
+          swapIndex = leftIndex;
+        }
+      }
+      if(rightIndex < length) {
+        rightElement = this.value[rightIndex];
+        if(rightElement > element && rightElement > leftElement) {
+          swapIndex = rightIndex;
+        }
+      }
+      if(!swapIndex) break;
+      this.swap(swapIndex, parentIndex);
+      parentIndex = swapIndex;
+    }
+  }
+
+  extractMax() {
+    this.swap(0, this.value.length - 1);
+    const removedElement = this.value.pop();
+    this.heapify();
+    return removedElement;
+  }
 }
 
 var maxBinaryHeap = new MaxBinaryHeap();
-maxBinaryHeap.insert(2);
-maxBinaryHeap.insert(7);
-maxBinaryHeap.insert(26);
-maxBinaryHeap.insert(25);
+// maxBinaryHeap.insert(2);
+// maxBinaryHeap.insert(7);
+// maxBinaryHeap.insert(26);
+// maxBinaryHeap.insert(25);
+maxBinaryHeap.insert(41);
+maxBinaryHeap.insert(39);
+maxBinaryHeap.insert(33);
+maxBinaryHeap.insert(18);
+maxBinaryHeap.insert(27);
+maxBinaryHeap.insert(12);
+maxBinaryHeap.insert(55);
---
+++ binary-heaps/priority-queue.js
@@ -0,0 +1,73 @@
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.value = [];
+  }
+
+  swap(index1, index2) {
+    let temp = this.value[index1];
+    this.value[index1] = this.value[index2];
+    this.value[index2] = temp;
+  }
+
+  // Recursion way
+  enqueue(value, priority) {
+    const newNode = new Node(value, priority);
+    this.value.push(newNode);
+    const bubbleUp = (index) => {
+      if (index < 0) return;
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.value[parentIndex] && this.value[index] && this.value[parentIndex].priority < this.value[index].priority) {
+        this.swap(index, parentIndex);
+      }
+      bubbleUp(parentIndex);
+    }
+    bubbleUp(this.value.length - 1);
+  }
+
+  heapify() {
+    let parentIndex = 0;
+    const length = this.value.length;
+    while (true) {
+      const element = this.value[parentIndex];
+      let swapIndex = null;
+      let leftElement = null;
+      let rightElement = null;
+      const leftIndex = 2 * parentIndex + 1;
+      const rightIndex = 2 * parentIndex + 2;
+      if (leftIndex < length) {
+        leftElement = this.value[leftIndex];
+        if (leftElement.priority > element.priority) {
+          swapIndex = leftIndex;
+        }
+      }
+      if (rightIndex < length) {
+        rightElement = this.value[rightIndex];
+        if (rightElement.priority > element.priority && rightElement.priority > leftElement.priority) {
+          swapIndex = rightIndex;
+        }
+      }
+      if (!swapIndex) break;
+      this.swap(swapIndex, parentIndex);
+      parentIndex = swapIndex;
+    }
+  }
+
+  dequeue() {
+    this.swap(0, this.value.length - 1);
+    const removedElement = this.value.pop();
+    this.heapify();
+    return removedElement;
+  }
+}
+
+var priorityQueue = new PriorityQueue();
+priorityQueue.enqueue("common cold", 1);
+priorityQueue.enqueue("gun shot wound", 5);
+priorityQueue.enqueue("high fever", 2);
